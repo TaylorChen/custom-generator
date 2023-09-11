16cbd8dd35c55737fb5384ae362efbcd02a29b01
chenrui <chenrui@aceplaying.com>
2023-09-11 16:40:50 +0800

    update

---
+++ src/main/java/com/sun/crm/common/pojo/User.java
@@ -0,0 +1,37 @@
+package com.sun.crm.common.pojo;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private Long id;
+
+    private String name;
+
+    private String pwd;
+
+    private static final long serialVersionUID = 1L;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd == null ? null : pwd.trim();
+    }
+}
